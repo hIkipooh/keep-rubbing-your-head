b9597fdf18352459d1fd1f12936d0f206afd1f26
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2019-02-12 10:18:14 +0900

    working on navbar left

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,27 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <navbar-left />
+    <text-and-c-l />
+    <index-right />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import NavbarLeft from "./components/NavbarLeft.vue";
+import TextAndCL from "./components/TextAndCL.vue";
+import IndexRight from "./components/IndexRight.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    "navbar-left": NavbarLeft,
+    "text-and-c-l": TextAndCL,
+    "index-right": IndexRight
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
